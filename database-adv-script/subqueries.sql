a9d6192f02e698d6e7242716fd456ac59b065b93
ayasabry-84 <ayasabryy017@gmail.com>
2025-07-04 09:25:47 +0300

    Add queries using subqueries: average rating per property and user bookings count

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,24 @@
+SELECT properties.name , properties.description , properties.location , properties.created_at
+FROM properties
+WHERE (
+		SELECT AVG(reviews.rating )
+		FROM reviews
+		WHERE reviews.property_id = properties.property_id
+	) > 4;
+    
+SELECT users.first_name , 
+	(
+	SELECT COUNT(*) AS booking_counts
+    FROM bookings
+    WHERE booking.user_id = users.user_id
+    )
+FROM users
+WHERE (
+	SELECT COUNT(*)
+    FROM bookings
+    WHERE booking.user_id = users.user_id
+    ) > 3
+    
+
+    
+    
