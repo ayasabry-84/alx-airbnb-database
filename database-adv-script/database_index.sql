6d60bacf4bf9e85bad58c6f8d56f9d89844cacb3
Aya Sabry <ayasabryy017@gmail.com>
2025-07-06 23:14:26 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,9 +1,14 @@
+-- Before indexing
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 'uuid-user-1';
+
+-- Create Indexes
 CREATE INDEX idx_users_email ON users(email);
-
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
-
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
-
 CREATE INDEX idx_properties_host_id ON properties(host_id);
-
 CREATE INDEX idx_bookings_created_at ON bookings(created_at);
+
+-- After indexing
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 'uuid-user-1';
